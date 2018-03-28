a4d6e0d1cbcc6845ab86757fb6444d235989e57f
Paul Lyon <plyon08@gmail.com>
2018-03-27 22:39:18 -0500

    Modified an existing page to fit better in the app

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,6 @@ Route::view('/', 'welcome');
 Route::get( '/auth0/callback', '\Auth0\Login\Auth0Controller@callback' )->name( 'auth0-callback' );
 Route::get('/login', 'Auth\Auth0IndexController@login' )->name( 'login' );
 Route::get('/logout', 'Auth\Auth0IndexController@logout' )->name( 'logout' )->middleware('auth');
-Route::get('/profile', 'Auth\Auth0IndexController@profile' )->name( 'profile' )->middleware('auth');
+Route::get('/add-recipe', function() {
+  return view('add-recipe');
+})->name('add-recipe');
