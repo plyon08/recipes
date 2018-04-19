cd11e6bb694b02c9fe80df991f6cab8286397bad
Paul Lyon <plyon08@gmail.com>
2018-04-19 11:59:52 -0500

    Added spacing between columns

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 	<div class='row'>
 		@foreach ($recipes as $r)
-			<div class='col-md-6 col-lg-3'>
+			<div class='col-md-6 col-lg-3 my-3'>
 				<div class='center recipe-index'>
 					<a href="/recipes/{{ $r->id }}">
 						<img class='recipe-img-index' src="{{ asset('storage/'. $r->image) }}" alt='No Image is Available' width='100%' height='auto' />
