abb34b7a2226b3e0c5a6df857aa88379c29b4e71
Paul Lyon <plyon08@gmail.com>
2018-04-18 19:36:50 -0500

    Updated edit template

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,69 +4,71 @@
 
 	@include('includes.error')
 
-	<h1>Add Recipe</h1>
-		{!! Form::model($recipe,['url' => '/recipes/'.$recipe->id,'method' => 'patch','class' => 'needs-validation','files' => true]) !!}
-			{!! Form::token() !!}
-			<div class="row">
-				<div class='col-xs-12'>
-					{!! Form::label('Recipe Name') !!}
-				</div>
-				<div class='col-xs-12 col-md-4 mb-3'>
-					{!! Form::text('recipeName',null,['placeholder' => 'Enter the name of the recipe','class' => 'form-control']) !!}
-				</div>
-			</div>
-		<hr>
-		<div class='row'>
-			<div class="col-xs-12 mb-3">
-				{!! Form::label('Ingredients') !!}
-				<small class="form-text text-muted">Ex. 1/2 cup sugar or 1 lb ham</small>
-			</div>
-			@for ($i = 1; $i < 16; $i++)
-				<div class='col-xs-12 col-md-4 mb-3'>
-					{!! Form::text('ingredient'.$i,null,['placeholder' => 'Ingredient '.$i,'class' => 'form-control']) !!}
-				</div>
-			@endfor
+	<div class='row'>
+		<div class='col'>
+			<h2>Add Recipe</h2>
+				{!! Form::model($recipe,['url' => '/recipes/'.$recipe->id,'method' => 'patch','class' => 'needs-validation','files' => true]) !!}
+					{!! Form::token() !!}
+					<div class="row my-5">
+						<div class='col-12'>
+							{!! Form::label('Recipe Name') !!}
+						</div>
+						<div class='col-12'>
+							{!! Form::text('recipeName',null,['placeholder' => 'Enter the name of the recipe','class' => 'form-control']) !!}
+						</div>
+					</div>
+					<div class='row my-5'>
+						<div class="col-12 mb-3">
+							{!! Form::label('Ingredients') !!}
+							<small class="form-text text-muted">Ex. 1/2 cup sugar or 1 lb ham</small>
+						</div>
 
-		</div>
-		<hr>
-		<div class='row'>
-			<div class="col-xs-12 mb-3">
-				{!! Form::label('Instructions') !!}
-			</div>
-			@for ($i = 1; $i < 16; $i++)
-				<div class='col-xs-12 col-md-4 mb-3'>
-					{!! Form::text('instruction'.$i,null,['placeholder' => 'Instruction '.$i,'class' => 'form-control']) !!}
+						@for ($i = 1; $i < 16; $i++)
+							<div class='col-12 col-md-6 col-lg-4 mb-3'>
+								{!! Form::text('ingredient'.$i,null,['placeholder' => 'Ingredient '.$i,'class' => 'form-control']) !!}
+							</div>
+						@endfor
+
+					</div>
+				<div class='row my-5'>
+					<div class="col-12 mb-3">
+						{!! Form::label('Instructions') !!}
+					</div>
+
+					@for ($i = 1; $i < 16; $i++)
+						<div class='col-12 col-md-6 col-lg-4 mb-3'>
+							{!! Form::text('instruction'.$i,null,['placeholder' => 'Instruction '.$i,'class' => 'form-control']) !!}
+						</div>
+					@endfor
+
 				</div>
-			@endfor
+				<div class='row my-5'>
+					<div class="col-12 mb-3">
+						<label for="image">Image Name</label>
+					</div>
+					<div class='col-12 mb-3'>
+						{!! Form::file('image') !!}
+						<p class='my-2'>
+							Current Image:
+						</p>
+						<p><img class='recipe-img' src="{{ asset('storage/'. $recipe->image) }}" alt='No image is currently used' width='100%' height='auto' /></p>
+					</div>
+				</div>
+				<div class='row my-5'>
+					<div class="col-12 mb-3">
+						<label for="tags">Tag</label>
+					</div>
+					<div class='col-12 col-md-3 mb-3'>
+						{!! Form::select('tag', ['Beef' => 'Beef','Chicken' => 'Chicken','Pork' => 'Pork','Ham' => 'Ham','Fish' => 'Fish','Pasta' => 'Pasta','Dessert' => 'Dessert'],null, ['placeholder' => 'Please pick a tag...','class' => 'form-control']) !!}
+					</div>
+				</div>
+				<div class='row justify-content-md-start my-5'>
+					<div class='col col-md-2 center'>
+						{!! Form::submit('Submit',['class' => 'btn btn-primary']) !!}
+					</div>
+				</div>
+			{!! Form::close() !!}
 		</div>
-		<hr>
-		<div class='row'>
-			<div class="col-xs-12 mb-3">
-				<label for="image">Image Name</label>
-			</div>
-			<div class='col-xs-12 col-md-4 mb-3'>
-				{!! Form::file('image') !!}
-				<p class='my-2'>
-					Current Image:
-				</p>
-				<p><img src="{{ asset('storage/'. $recipe->image) }}" alt='No image is currently used' width='300px' height='auto' /></p>
-			</div>
-		</div>
-		<hr>
-		<div class='row'>
-			<div class="col-xs-12 mb-3">
-				<label for="tags">Tag</label>
-			</div>
-			<div class='col-xs-12 col-md-4 mb-3'>
-				{!! Form::select('tag', ['Beef' => 'Beef','Chicken' => 'Chicken','Pork' => 'Pork','Ham' => 'Ham','Fish' => 'Fish','Pasta' => 'Pasta','Dessert' => 'Dessert'],null, ['placeholder' => 'Please pick a tag...','class' => 'form-control']) !!}
-			</div>
-		</div>
-		<hr>
-		<div class='row mb-5'>
-			<div class='col-xs-12 col-md-4'>
-				{!! Form::submit('Submit',['class' => 'btn btn-primary']) !!}
-			</div>
-		</div>
-	{!! Form::close() !!}
+	</div>
 
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 			<div class='col-md-6 col-lg-3'>
 				<div class='center recipe-index'>
 					<a href="/recipes/{{ $r->id }}">
-						<img class='recipe-img' src="{{ asset('storage/'. $r->image) }}" alt='No Image is Available' width='100%' height='auto' />
+						<img class='recipe-img-index' src="{{ asset('storage/'. $r->image) }}" alt='No Image is Available' width='100%' height='auto' />
 					</a>
 					<div class='recipe-name-index'>{{ $r->recipeName }}</div>
 				</div>
