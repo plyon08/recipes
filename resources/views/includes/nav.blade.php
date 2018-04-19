91097d930d2083fd9036becad515987a1fb9d43a
Paul Lyon <plyon08@gmail.com>
2018-04-19 12:02:10 -0500

    Added a login status to show username if logged in

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -2,6 +2,13 @@
   <section class='col'>
     <nav class="navbar navbar-expand-lg navbar-dark">
       <a class="navbar-brand" href="{{ url('/') }}">Recipes</a>
+      <div class='login-status'>
+        @if (Auth::check())
+          {{ Auth::user()->nickname }}
+        @else
+          Not Logged In
+        @endif
+      </div>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
