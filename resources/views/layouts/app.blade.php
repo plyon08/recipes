d51e09517e626a5bb63b36e913a84cc9f9aa6b8d
Paul Lyon <plyon08@gmail.com>
2018-04-23 16:20:59 -0500

    Changed helper function for css and js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
 
 	<title>Recipes</title>
 
-	<link rel="stylesheet" href="{{ mix('/css/app.css') }}" />
-	<script src="{{ mix('/js/app.js') }} "></script>
+	<link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+	<script src="{{ asset('js/app.js') }} "></script>
 
 </head>
 <body class='container-fluid mb-4'>
